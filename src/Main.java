d79adad1d96d87ad0a23001f29c2a041c47e751a
Mantvydas Deltuva <deltmante@gmail.com>
2023-09-13 17:21:40 +0300

    Made modifications that would allow for years input to be not only four digits, but also three, two and one digit. Also negative years are invalid

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,33 @@ import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-            printBonusDatesBetween(2010, 2015);
-            printBonusDatesBetween(1900, 1900);
+        printBonusDatesBetween(0, 9);
+        printBonusDatesBetween(0, 99);
+        printBonusDatesBetween(0, 999);
+        printBonusDatesBetween(0, 9999);
+        printBonusDatesBetween(10, 99);
+        printBonusDatesBetween(10, 999);
+        printBonusDatesBetween(10, 9999);
+        printBonusDatesBetween(100, 999);
+        printBonusDatesBetween(100, 9999);
+        printBonusDatesBetween(1000, 9999);
+        printBonusDatesBetween(2010, 2015);
+        printBonusDatesBetween(-1000, -100);
+        printBonusDatesBetween(2010, 2010);
     }
 
     public static void printBonusDatesBetween(int fromYear, int toYear) {
-        if (fromYear >= toYear)
+        if (fromYear >= toYear || fromYear < 0)
             return;
 
-        LocalDate fromDate = LocalDate.parse(fromYear+"-01-01");
-        LocalDate toDate = LocalDate.parse(toYear+"-01-01");
+        int fromSizeAdd = 4 - Integer.toString(fromYear).length();
+        int toSizeAdd = 4 - Integer.toString(toYear).length();
+
+        String fromParse = new String(new char[fromSizeAdd]).replace('\0', '0') + fromYear + "-01-01";
+        String toParse = new String(new char[toSizeAdd]).replace('\0', '0') + toYear + "-01-01";
+
+        LocalDate fromDate = LocalDate.parse(fromParse);
+        LocalDate toDate = LocalDate.parse(toParse);
 
         while (!fromDate.equals(toDate)) {
             String fromDateString = fromDate.toString().replace("-", "");
